483389c246a4da6497489dfacc56c3704ef1a977
Charles F. Munat <charles.munat@gmail.com>
2016-02-13 12:50:03 +1300

    Add Run Tests button

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 
-import { Col, Grid, Row } from 'react-bootstrap'
+import { Button, Col, Grid, Row } from 'react-bootstrap'
 
 import TestRunner from './TestRunner.jsx!'
 
@@ -8,12 +8,40 @@ import tests from '../tests.js'
 
 class App extends Component {
 
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      tests: []
+    }
+  }
+
+  runTests () {
+    this.setState({
+      tests: tests
+    })
+  }
+
   render () {
     return <Grid>
       <Row>
         <Col xs={10} xsOffset={1}>
+          <Button
+            onClick={this.runTests.bind(this)}
+            bsStyle='success'
+            bsSize='small'
+            style={{float: 'right'}}
+          >Run Tests</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10} xsOffset={1}>
           <h1>NRI Test Runner</h1>
-          <TestRunner tests={tests}/>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={10} xsOffset={1}>
+          <TestRunner tests={this.state.tests}/>
         </Col>
       </Row>
     </Grid>
